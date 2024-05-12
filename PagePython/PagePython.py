533f935b86648e5378e05d552dad253ac8878d51
MatTheTab <mateusztabaszewski@gmail.com>
2024-05-12 17:40:41 +0200

    Add general purpose utils

--- PagePython/PagePython.py
+++ PagePython/PagePython.py
@@ -1,9 +1,10 @@
 from cassandra.cluster import Cluster
 from cassandra.policies import RetryPolicy, ExponentialReconnectionPolicy
+from utils.select_utils import *
 import uuid
 import time
 
-cluster = Cluster(['172.20.0.2'])
+cluster = Cluster(['172.23.0.2'])
 session = cluster.connect()
 
 # Set retry and reconnection policies with longer timeout
@@ -23,18 +24,18 @@ table_query = """
         user_id INT,
         reservation_start TIMESTAMP,
         reservation_end TIMESTAMP,
-        book_id UUID,
+        book_id INT,
         book_name TEXT,
         book_genre TEXT,
-        PRIMARY KEY (reservation_id, user_id)
+        PRIMARY KEY (reservation_id)
     );
 """
 session.execute(table_query, timeout=120)
 print("Created Table")
 
 reservation_id = uuid.uuid4()
-user_id = 1  # Assuming user_id as integer
-book_id = uuid.uuid4()
+user_id = 1
+book_id = 1
 insert_query = """
     INSERT INTO reservations (reservation_id, user_id, reservation_start, reservation_end, book_id, book_name, book_genre)
     VALUES  (%s, %s, toTimestamp(now()), toTimestamp(now()) + 1d, %s, 'Catcher in the Ray', 'Drama'); 
@@ -51,6 +52,52 @@ selection_query = """
 result = session.execute(selection_query)
 for row in result:
     print(row)
+
+################### (SMALL) TESTS ###################
+print()
+print("Commensing Function Tests")
+print()
+
+make_reservation(session = session, user_id = 1, book_id = 2)
+make_reservation(session = session, user_id = 3, book_id = 5)
+print("Reservations Made")
+
+time.sleep(10)
+results = select_all_reservations(session)
+print()
+print("All Reservations")
+for row in results:
+    print(row)
+
+print()
+print("User Reservations for user_id = 1")
+results = select_all_reservations(session = session)
+user_reservation_id = None 
+for row in results:
+    if row.user_id == 1:
+        print(row)
+        user_reservation_id = row.reservation_id
+
+print()
+print("Updating Reservation Time")
+update_reservation_end_time(session = session, reservation_id = user_reservation_id, new_end_time = "toTimestamp(now()) + 10d")
+time.sleep(10)
+results = select_all_reservations(session = session)
+for row in results:
+    print(row)
+
+print()
+print("Cancelling Reservations")
+cancel_reservation(session = session, reservation_id = user_reservation_id)
+time.sleep(10)
+results = select_all_reservations(session = session)
+for row in results:
+    print(row)
+
+print()
+print("Tests Passed")
+print()
+################### END OF TESTS ###################
 
 table_deletion_query = """ 
     DROP TABLE IF EXISTS reservations;
