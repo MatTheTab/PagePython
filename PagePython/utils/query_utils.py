cc67bac7cfaf0af60f3f5c23885769f3474387f4
MatTheTab <mateusztabaszewski@gmail.com>
2024-05-28 11:52:20 +0200

    Fix concurency bugs

--- PagePython/utils/query_utils.py
+++ PagePython/utils/query_utils.py
@@ -1,4 +1,5 @@
 from cassandra import InvalidRequest, ReadTimeout, ReadFailure
+from cassandra.protocol import SyntaxException
 import uuid
 
 # Function Descriptions:
@@ -161,7 +162,7 @@ def add_reservation(session, reservation_id, user_id, user_name, book_name, book
     """
     try:
         book = get_book(session, book_id = book_id)
-        if not book.is_reserved:
+        if book is not None and not book.is_reserved:
             set_book_reserved(session, book_id=book_id, reserved = True, timeout = timeout)
             append_user_reservation(session, user_id, user_name, reservation_id, timeout = timeout)
             session.execute(insert_reservation_query, [reservation_id, user_id, user_name, book_name, book_id], timeout=timeout)
@@ -175,14 +176,14 @@ def update_reservation_book(session, reservation_id, book_id, timeout = 120):
     try:
         reservation = get_reservation_by_id(session, reservation_id, timeout = timeout)
         book = get_book(session, book_id, timeout = timeout)
-        if not book.is_reserved:
+        if book is not None and reservation is not None and not book.is_reserved:
             new_book_name = book.book_name
             past_book_id = reservation.book_id
             set_book_reserved(session, past_book_id, reserved = False, timeout = timeout)
             set_book_reserved(session, book_id, reserved = True, timeout = timeout)
             session.execute(update_reservation_query, [book_id, new_book_name, reservation_id], timeout = timeout)
             return True
-    except InvalidRequest as e:
+    except (InvalidRequest, SyntaxException) as e:
         print("Error occurred while updating a reservation:", e)
 
     return False
@@ -203,7 +204,7 @@ def update_reservation_user(session, reservation_id, user_id, timeout = 120):
             session.execute(update_reservation_query, [user_id, new_user_name, reservation_id], timeout = timeout)
             return True
         
-    except InvalidRequest as e:
+    except (InvalidRequest, SyntaxException) as e:
         print("Error occurred while updating a reservation:", e)
 
     return False
@@ -223,14 +224,14 @@ def update_username(session, user_id, user_name, timeout = 120):
                 user_reservation_id = reservation.reservation_id
                 session.execute(update_user_name_reservation_query, [user_name, user_reservation_id], timeout = timeout)
         session.execute(update_user_name_query, [user_name, user_id], timeout = timeout)
-    except InvalidRequest as e:
+    except (InvalidRequest, SyntaxException) as e:
         print("Error occurred while updating a reservation:", e)
 
 def delete_user_reservation(session, user_id, reservation_id, timeout = 120):
     user_reservation_delete_query = f"""UPDATE users SET reservation_ids_list = reservation_ids_list - [{reservation_id}] WHERE user_id = {user_id}"""
     try:
         session.execute(user_reservation_delete_query, timeout=timeout)
-    except InvalidRequest as e:
+    except (InvalidRequest, SyntaxException) as e:
         print("Error occurred while removing reservation ID from user:", e)
         raise e
 
@@ -240,12 +241,13 @@ def cancel_reservation(session, reservation_id, timeout = 120):
     """
     try:
         reservation = get_reservation_by_id(session, reservation_id, timeout = timeout)
-        user_id = reservation.user_id
-        book_id = reservation.book_id
-        set_book_reserved(session, book_id, reserved = False, timeout = timeout)
-        delete_user_reservation(session, user_id, reservation_id, timeout = timeout)
-        session.execute(reservation_cancel_query, [reservation_id], timeout = timeout)
-    except InvalidRequest as e:
+        if reservation is not None:
+            user_id = reservation.user_id
+            book_id = reservation.book_id
+            set_book_reserved(session, book_id, reserved = False, timeout = timeout)
+            delete_user_reservation(session, user_id, reservation_id, timeout = timeout)
+            session.execute(reservation_cancel_query, [reservation_id], timeout = timeout)
+    except (InvalidRequest, SyntaxException) as e:
         print("Error canceling the reservation: ", e)
 
 def get_all_reservations(session, timeout = 120):
@@ -278,7 +280,7 @@ def get_reservation_by_id(session, reservation_id, timeout=120):
         if reservation is None:
             raise ValueError("No reservation found with ID: {}".format(reservation_id))
         return reservation
-    except (InvalidRequest, ReadTimeout, ReadFailure, ValueError) as e:
+    except (InvalidRequest, ReadTimeout, ReadFailure, ValueError, SyntaxException) as e:
         print("Error occurred while fetching the reservation:", e)
         return None
     
@@ -291,7 +293,7 @@ def get_book(session, book_id, timeout=120):
         if book is None:
             raise ValueError("No book found with ID: {}".format(book_id))
         return book
-    except (InvalidRequest, ReadTimeout, ReadFailure, ValueError) as e:
+    except (InvalidRequest, ReadTimeout, ReadFailure, ValueError, SyntaxException) as e:
         print("Error occurred while fetching the book:", e)
         return None
 
@@ -304,7 +306,7 @@ def get_user(session, user_id, timeout=120, print_error_message = True):
         if user is None:
             raise ValueError("No user found with ID: {}".format(user_id))
         return user
-    except (InvalidRequest, ReadTimeout, ReadFailure, ValueError) as e:
+    except (InvalidRequest, ReadTimeout, ReadFailure, ValueError, SyntaxException) as e:
         if print_error_message:
             print("Error occurred while fetching the user:", e)
         return None
